e128615def69509df0f41c417f1ca5061b805dc5
J. Brandon Walker <88942620+walkerjb02@users.noreply.github.com>
2021-11-23 12:37:25 -0500

    Create UDP.py

---
+++ UDP.py
@@ -0,0 +1,71 @@
+import socket
+import time
+import threading as tr
+
+class server:
+    def __init__(self):
+        self.serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.serverSocket.bind(('', 0))
+        self.serverIP = '10.0.0.46'
+        self.serverPort = self.serverSocket.getsockname()[1]
+        print("serverIP:\t" + self.serverIP)
+        print("serverPort:\t" + str(self.serverPort))
+        self.clientportmessage = []
+        self.message = str(self.serverPort)
+        self.clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    def connectionestablish(self):
+        n = 0
+        while 1:
+            for i in range(0, int(self.serverPort)):
+                self.clientSocket.sendto(self.message.encode(), (self.serverIP, i))
+            for j in range(int(self.serverPort + 1), 65535):
+                self.clientSocket.sendto(self.message.encode(), (self.serverIP, j))
+            self.port, clientAddress = self.serverSocket.recvfrom(2048)
+            if self.port:
+                self.port = str(self.port.decode('UTF-8'))
+                itera = '123456789'
+                for a in itera:
+                    if a in str(self.port) and len(self.port) == 5:
+                        self.clientportmessage.append(self.port)
+                    else:
+                        pass
+            self.clientportmessage = list(set(self.clientportmessage))
+            if n < len(self.clientportmessage):
+                print(f'Connection Established with {self.port}')
+                n += 1
+                time.sleep(5)
+            else:
+                pass
+
+    def messagesend(self):
+        while 1:
+            message = input('Message?\n')
+            n = 0
+            while n < 2:
+                for j in self.clientportmessage:
+                    self.clientSocket.sendto(str(message).encode(), (self.serverIP, int(j)))
+                n += 1
+
+
+    def messagereceive(self):
+        while 1:
+            shown = False
+            while shown == False:
+                self.inbound, clientAddress = self.serverSocket.recvfrom(2048)
+                if self.inbound:
+                    self.inbound = str(self.inbound)
+                    itera = 'aeiouy'
+                    for a in itera:
+                        if a in str(self.inbound):
+                            self.inbound = str(self.inbound)
+                            print(str(self.inbound))
+                shown = True
+
+
+s = server()
+
+tr.Thread(target=s.connectionestablish).start()
+time.sleep(30)
+tr.Thread(target=s.messagereceive).start()
+tr.Thread(target=s.messagesend).start()
